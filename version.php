7c93597ad68dfcf5e5062f2d8b3fd89ea98b8c81
Matt Davidson <davidso1@rose-hulman.edu>
2017-05-04 09:10:46 -0400

    Moodle 3.3 release

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
-$plugin->version   = 2017032901;                // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017050400;                // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014111000;                // Requires this Moodle version.
 $plugin->component = 'tool_coursearchiver';     // Full name of the plugin (used for diagnostics).
-$plugin->release  = '3.2.2 (Build: 2016090200)';
+$plugin->release  = '3.3.1 (Build: 2016090200)';
 $plugin->maturity  = MATURITY_STABLE;
