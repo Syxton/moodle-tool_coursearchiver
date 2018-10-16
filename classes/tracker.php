9b26d7f631b3226d422404ad050dc296cce2e5f6
Matt Davidson <davidso1@rose-hulman.edu>
2018-10-16 10:07:42 -0400

    Fix code style problem

--- classes/tracker.php
+++ classes/tracker.php
@@ -415,8 +415,12 @@ class tool_coursearchiver_tracker {
         if ($this->outputmode == self::OUTPUT_CLI) {
             switch ($this->mode) {
                 case tool_coursearchiver_processor::MODE_COURSELIST:
-                    $cliicon = empty($data->visible) ? get_string('hidden', 'tool_coursearchiver') : get_string('visible', 'tool_coursearchiver');
-                    $empty = $this->empty ? ";".get_string('empty', 'tool_coursearchiver')." " : " ";
+                    if (empty($data->visible)) {
+                        $cliicon = get_string('hidden', 'tool_coursearchiver');
+                    } else {
+                        $cliicon = get_string('visible', 'tool_coursearchiver');
+                    }
+                    $empty = $this->empty ? ";" . get_string('empty', 'tool_coursearchiver') . " " : " ";
                     $date = get_string('never', 'tool_coursearchiver');
                     if (!empty($data->timeaccess)) {
                         $date = date("m/d/y", $data->timeaccess);
