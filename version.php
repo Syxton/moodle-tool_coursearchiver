9c2398f014efd2fb5a9323158760778f42a4874a
Mark Sharp <mark.sharp@solent.ac.uk>
2025-11-28 09:59:56 +0000

    Moodle 4.5+ compatibility

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->version   = 2025102100;                // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2025040800;                // Requires this Moodle version.
+$plugin->requires  = 2024100700;                // Requires this Moodle version.
 $plugin->component = 'tool_coursearchiver';     // Full name of the plugin (used for diagnostics).
 $plugin->release  = '5.0.1';
 $plugin->maturity  = MATURITY_STABLE;
