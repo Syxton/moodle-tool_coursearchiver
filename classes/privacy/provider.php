ba6fbf44a05adfba937ae7bbcb93c281af4030f5
Matt Davidson <davidso1@rose-hulman.edu>
2026-05-07 11:14:08 -0400

    fix code check and expand testing

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -37,7 +37,7 @@ use core_privacy\local\request\userlist;
  * @copyright  2015 Matthew Davidson
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\request\data_provider, \core_privacy\local\metadata\provider {
+class provider implements \core_privacy\local\metadata\provider, \core_privacy\local\request\data_provider {
     /**
      * Returns metadata about this plugin's privacy policy.
      *
